daf98f1aaa842e97f09a35cd76e66e6ae5d04f69
zack1212-22 <ffgutg5567ggugh@gmail.com>
2026-06-16 03:02:53 +0000

    ZACK.py

---
+++ ZACK.py
@@ -0,0 +1,7 @@
+num1 = float(input("ใส่ความกว้าง: "))
+num2 = float(input("ใส่ความยาว: "))
+result = num1 * num2
+print("-" * 30)
+print(f"พื้นที่สี่เหลี่ยมมุมฉาก: {round(result, 4):,}")
+print("-" * 30)
+print("Author by ZACK M.6/9")
